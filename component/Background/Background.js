77cb64e978d4a8048d76022f405ed37dcef0df17
Azim Abdumutalibov <azimabdumutalibov21@gmail.com>
2022-05-25 17:45:18 +0500

    bag

--- component/Background/Background.js
+++ component/Background/Background.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import styles from '../../styles/Background.module.css';
 import { Black } from './Styled';
 
 const Background = prop => (
     <>
     <Black onClick={prop.click}/>
     </>
-    // <div className={styles.back} onClick={prop.click}/>
 )
 
 export default Background;
