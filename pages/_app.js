7634a267bcbcd0f963c6278a15488acf30d7a70d
Azim Abdumutalibov <azimabdumutalibov21@gmail.com>
2022-05-26 14:36:17 +0500

    netlify form

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-// import Layout from '../component/Layout'
+import Layout from '../component/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
 
   return (
-  // <Layout>
+  <Layout>
+  <Component {...pageProps} />
 
-  // </Layout>
-    <Component {...pageProps} />
+   </Layout>
     
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
         />
       </Head>
       {/* <Editor/> */}
-      <Layout />
+      {/* <Layout /> */}
     </div>
   );
 }
